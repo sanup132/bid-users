767e280b3bb39de1ae640133db9232886cd49b9f
Sanup <sanup.cse@gmail.com>
2022-11-16 13:51:54 +0530

    commit

---
+++ bid-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bid-users',
+  templateUrl: './bid-users.component.html',
+  styleUrls: ['./bid-users.component.css']
+})
+export class BidUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
